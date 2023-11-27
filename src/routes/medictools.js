8dd7b50c8dcd4ab0c1f36606a94e6cc0970dd068
Kenay19 <omarlazor@hotmail.com>
2023-11-26 21:57:24 -0600

    a

--- src/routes/medictools.js
+++ src/routes/medictools.js
@@ -196,7 +196,7 @@ router.post('/deleteProduct',async(req,res) => {
       await pool.query('DELETE FROM Imagenes WHERE ruta = ?',[pathImage[i].ruta])
 
     }
-    const result = await pool.query('DELETE FROM equipoMedico WHERE idEquipoMedico = ?',[idEquipoMedico])
+    const result = await pool.query('DELETE FROM EquipoMedico WHERE idEquipoMedico = ?',[idEquipoMedico])
     res.json(result);
   } catch (error) {
     res.json({error: error.message, sql: error.sql});
